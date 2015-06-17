5ef350e9c2a35abcaffb152c2918d08dda9ec04a
Tuan Nguyen <tuan@brandaffinity.net>
2015-06-17 13:44:45 -0700

    initial search format.

--- client/js/component/search.js
+++ client/js/component/search.js
@@ -6,11 +6,38 @@ AppStore = require('../store/appStore'),
 React = require('react');
 
 var Mention = React.createClass({
+  getSnippetText: function(snippets){
+    var text = '';
+    if (!snippets){
+      return text;
+    }
+
+    for (var i = 0; i < snippets.length; i++) {
+      text += snippets[i].text;
+    };
+
+    return text;
+  },
   render: function() {
-    console.log(AppConstant.ACTION_CREATE);
-    var child = React.DOM.span(null,this.props.data.programName);
-    var elm = React.DOM.div({className:'mentionList'}, child);
-    return elm;
+    
+    //add background image
+    var backgroundStyle = {backgroundImage:'url(' + this.props.data.programLiveImage + ')',
+                           backgroundSize: 'cover' }
+    var backgroundImage = React.DOM.div({className: 'prog-avtr', style:backgroundStyle});
+
+    //add porgram name
+    var arrowIcon  = React.DOM.a({className: 'blue-play'});
+    var programName  = React.DOM.div({className: 'm5 bold'}, React.DOM.a(null,this.props.data.programName));
+    var airDate = React.DOM.i({className: 'mention-air-date'}, 'Air date: ' + this.props.data.mediaStartTime);
+    var programNameContainer = React.DOM.div({className: 'prog-title'},arrowIcon, programName,airDate);
+
+    //add mention snippet
+    var mentionSnippet = React.DOM.p({className: 'mention-snippet-text'}, React.DOM.span({className:'cur-point ui-snip-text'}, this.getSnippetText(this.props.data.mentionSnippet)));
+
+    var holder = React.DOM.div({className: 'ui-au-holder'},backgroundImage, programNameContainer, mentionSnippet);
+    var container = React.DOM.li({className:'ui-search-item'}, holder);
+
+    return container;
   }
 });
 
@@ -19,23 +46,11 @@ var MentionList = React.createClass({
     var mentionNodes = this.props.data.map(function (mention) {
       return React.createElement(Mention, {data:mention});
     });
-    return React.DOM.div({className: 'mentionList'}, mentionNodes);
+    return React.DOM.ul({className: 'results'}, mentionNodes);
   }
 });
 
 module.exports = React.createClass({
-  loadCommentsFromServer: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      success: function(data) {
-        this.setState({data: data.records});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
   onChange: function() {
     this.setState({data: AppStore.getSearchResults()});
   },
